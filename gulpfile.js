38a25a7cf8abd6af13a4e6d453f79f7a05adef59
Théotime <hobbes@theoti.me>
2017-07-10 22:49:42 +0200

    Add CSS workflow

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const gulp = require('gulp');
+const less = require('gulp-less');
+const shell = require('gulp-shell');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const paths = {
+    build: {
+        watch: 'src/*.ml'
+    },
+    less: {
+        src: 'src/less/style.less',
+        dest: 'static/',
+        watch: 'src/less/**'
+    }
+};
+
+gulp.task('build', shell.task([
+  'make'
+]));
+
+gulp.task('less', function () {
+    gulp.src(paths.less.src)
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest(paths.less.dest))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "static",
+            directory: true
+        },
+        open: false
+    });
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('build-watch', ['build'], function() {
+    browserSync.reload();
+});
+
+gulp.task('watch', ['build', 'less', 'browser-sync'], function () {
+    gulp.watch(paths.less.watch, ['less']);
+    gulp.watch(paths.build.watch, ['build-watch']);
+});
+
+gulp.task('default', ['less', 'build']);
